946d702bfcfcabcab7877ec983dd32ef045b2311
Harshal Chavan <91681985+hcharshal31@users.noreply.github.com>
2024-12-17 14:48:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
+	let chunks = [];
+	let sumArray =[]; 
+	let sum = 0;
+	for(let i = 0; i < arr.length; i++){
+		sum += arr[i];
+		if(sum <= n){
+			sumArray.push(arr[i]);
+		}else {
+			chunks.push(sumArray);
+			sum = 0;
+			sumArray = [];
+			i = i;
+		}
+	}
+	return chunks;
   // Write your code here
 };
 
-const n = prompt("Enter n: ");
-alert(JSON.stringify(divide(arr, n)));
+// const n = prompt("Enter n: ");
+// alert(JSON.stringify(divide(arr, n)));
